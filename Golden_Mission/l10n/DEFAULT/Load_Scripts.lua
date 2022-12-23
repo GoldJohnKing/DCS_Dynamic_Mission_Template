9582c1b981025394ea890c9411a6c882407ffb8f
Gold John King <GoldJohnKing@Live.cn>
2022-12-23 20:51:56 +0800

    Initial version that can be played

---
+++ Golden_Mission/l10n/DEFAULT/Load_Scripts.lua
@@ -0,0 +1,24 @@
+local script_path = "D:/GitRepos/DCS_Dynamic_Mission_Template/Golden_Scripts/"
+
+local script_list =
+{
+    -- Load order must be correct
+    "Moose.lua",
+    "Mission.lua",
+}
+
+local function load_scripts(path, list)
+    for index, value in ipairs(list) do
+        dofile(path .. value)
+    end
+end
+
+if lfs then
+    script_path = lfs.writedir() .. "Missions/Scripts/"
+
+    env.info("Script Loader: LFS available, using relative script load path: " .. script_path)
+else
+    env.info("Script Loader: LFS not available, using default script load path: " .. script_path)
+end
+
+load_scripts(script_path, script_list)
---
+++ Golden_Scripts/Mission.lua
@@ -0,0 +1,251 @@
+local table = table
+local math = math
+
+-- Utils
+
+local function get_random(tb)
+    local keys = {}
+
+    for key, vaule in pairs(tb) do
+        table.insert(keys, key)
+    end
+
+    return tb[keys[math.random(#keys)]]
+end
+
+local function message_to_all(_text, _lasts_time)
+    MESSAGE:New(_text, _lasts_time):ToAll()
+end
+
+-- End of Utils
+
+message_to_all("Mission.lua Loading", 30) -- Debug
+
+-- Enums
+
+local SIDE = {
+    BLUE = coalition.side.BLUE,
+    RED = coalition.side.RED,
+    NEUTRAL = coalition.side.NEUTRAL,
+}
+
+local COUNTRY = {
+    [SIDE.BLUE] = country.id.CJTF_BLUE,
+    [SIDE.RED] = country.id.CJTF_RED,
+}
+
+local ZONE_COLOR = {
+    [SIDE.BLUE] = { 0, 0, 0.8 },
+    [SIDE.RED] = { 0.8, 0, 0 },
+    [SIDE.NEUTRAL] = { 0.8, 0.8, 0.8 },
+}
+
+-- End of Enums
+
+-- Definitions
+
+local def_zone_list = {
+    sharjah = {
+        name = "Sharjah",
+        side = SIDE.BLUE,
+    },
+    dubai = {
+        name = "Dubai",
+        side = SIDE.RED,
+    },
+}
+
+local group_template_list = {
+    -- Armory
+    "BTR80",
+    "BTRRD",
+    "HMMWV",
+    "BMP3",
+    "BTR82A",
+    "M1126",
+    "M2A2",
+    "Warrior",
+    "Challenger",
+    "Leclerc",
+    "Leopard2A6M",
+    "M1A2",
+    "MerkavaIV",
+    "T72B3",
+    "T80U",
+    "T90",
+    "Type59",
+    "StrykerMGS",
+    "ZBD04A",
+    "ZTZ96B",
+
+    -- Artillery
+    "BM27",
+    "BM21",
+    "PLZ05",
+    "2S3",
+    "M109",
+    "2S9",
+
+    -- Unarmed
+    "Tigr",
+    "M978",
+    "GAZ66",
+    "KAMAZ43101",
+    "KrAZ6322",
+    "M939",
+    "Ural375",
+    "Ural4320T",
+}
+
+-- End of Definitions
+
+-- Zone Initialization
+
+local zone_list = {
+    [SIDE.BLUE] = {
+    },
+    [SIDE.RED] = {
+    },
+    [SIDE.NEUTRAL]= {
+    },
+}
+
+for key, value in pairs(def_zone_list) do
+    table.insert(zone_list[value.side], ZONE:FindByName(value.name))
+end
+
+local zone_set = {
+    [SIDE.BLUE] = SET_ZONE:New(),
+    [SIDE.RED] = SET_ZONE:New(),
+    [SIDE.NEUTRAL] = SET_ZONE:New(),
+}
+
+for key, value in pairs(def_zone_list) do
+    zone_set[value.side]:AddZonesByName(value.name)
+end
+
+for key, value in pairs(zone_set) do
+    value:DrawZone(-1, { 1, 1, 1 }, 1, ZONE_COLOR[key], 0.25, 1, true)
+end
+
+-- End of Zone Initialization
+
+-- Group Initialization
+
+local group_list = {
+    [SIDE.BLUE] = {
+    },
+    [SIDE.RED] = {
+    },
+}
+
+local group_count = 
+{
+    [SIDE.BLUE] = 0,
+    [SIDE.RED] = 0,
+}
+
+-- Destroy all groups templates at mission start
+
+SET_GROUP:New():AddGroupsByName(group_template_list):ForEachGroup(
+    function(group)
+        group:Destroy(false)
+    end
+)
+
+-- End of Group Initialization
+
+-- Group Spawn
+
+local function on_group_spawn(group, route_target_zone)
+    group:TaskRouteToZone(route_target_zone, true, 30, "Off Road")
+end
+
+local function group_spawn_random(_side)
+    local _group_prefix = nil
+    local _target_zone_list = nil
+
+    if _side == SIDE.BLUE then
+        _group_prefix = "B-"
+        _target_zone_list = zone_list[SIDE.RED]
+    else
+        _group_prefix = "R-"
+        _target_zone_list = zone_list[SIDE.BLUE]
+    end
+
+    for key, value in pairs(zone_list[SIDE.NEUTRAL]) do
+        table.insert(_target_zone_list, value)
+    end
+
+    local _spawn_template = get_random(group_template_list)
+    local _spawn_zone = get_random(zone_list[_side])
+
+    local _group_count = group_count[_side]
+    group_count[_side] = group_count[_side] + 1
+
+    local _country = COUNTRY[_side]
+    
+    local target_zone = get_random(_target_zone_list)
+
+    _group_prefix = _group_prefix .. _spawn_template .. "-" .. _spawn_zone:GetName() .. "-" .. target_zone:GetName() .. "-" .. _group_count
+
+    local _group = SPAWN:NewWithAlias(_spawn_template, _group_prefix)
+        :InitCoalition(_side)
+        :InitCountry(_country)
+        :OnSpawnGroup(on_group_spawn, target_zone)
+        :SpawnInZone(_spawn_zone, true)
+
+    table.insert(group_list[_side], _group)
+
+    -- Debug Message
+    local _side_name = nil
+    if _side == SIDE.BLUE then
+        _side_name = "Blue"
+    else
+        _side_name = "Red"
+    end
+    message_to_all("Debug: " .. _group_prefix .. "has been spawned.\n" .. _side_name .. " side now has " .. #group_list[_side] .. " groups.", 15)
+    -- End of Debug Message
+end
+
+TIMER:New(group_spawn_random, SIDE.BLUE):Start(90, 180)
+TIMER:New(group_spawn_random, SIDE.RED):Start(90, 180)
+
+-- End of Group Spawn
+
+-- Scheduled Restart
+
+local restart_time = 21600 -- 6 hours -- 10800 = 3 hours
+local restart_hint_time = { 60, 180, 300, 900 }
+local restart_hint_lasts_time = 90
+
+for key, value in pairs(restart_hint_time) do -- Restart hint
+	TIMER:New(message_to_all, "服务器将于" .. value / 60 .. "分钟后定时重启！", restart_hint_lasts_time):Start(restart_time - value)
+end
+
+TIMER:New(message_to_all, "服务器即将定时重启！", restart_hint_lasts_time):Start(restart_time - 15)
+
+USERFLAG:New("FlagScheduledRestart"):Set(false):Set(true, restart_time)
+
+-- End of Scheduled Restart
+
+-- Server Message
+
+local server_message_lasts_time = 60
+local server_message_delay = 30
+local server_message_duration = 1800
+local server_message_text = {
+    "欢迎来到 [#1金家寨] <高加索：攻占模式> 服务器！\n\nQQ群：750508967\nKOOK(开黑啦)语音频道：95367853\n\n强烈建议各位玩家加入KOOK语音频道，加强沟通，相互配合，以提升作战效率！\n",
+    "===== 服务器公告 =====\n\n本服务器设有自动封禁系统，攻击友军后请尽快道歉并获取谅解。如果您不幸被自动封禁，请在QQ群内联系老金解封。\n",
+    "本服务器以大规模PVE作战为主要玩法，服务端的运算压力显著高于其他轻量化的PVE或PVP玩法。\n由于硬件性能的限制，本服务器难以承载超过28名玩家同时在线，否则极易发生卡顿和异常。\n",
+    "服务器的日常运营和硬件迭代都离不开庞大的资金支持。如果您觉得本服务器很好玩，欢迎进行赞助！\n请注意，捐助本服务器不会为您带来任何意义上的特权，请理性捐赠，量力而行，谢谢！\n",
+}
+
+for key, value in pairs(server_message_text) do
+    TIMER:New(message_to_all, value, server_message_lasts_time):Start(server_message_delay, server_message_duration)
+    server_message_delay = server_message_delay + 5
+end
+
+-- End of Server Message
+
+message_to_all("Mission.lua Loaded", 30) -- Debug
